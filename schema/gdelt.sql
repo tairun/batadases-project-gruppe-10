a299b7afac8acccf8b91bc72396633876e0462ad
Luc Kury <luc.kury@unibas.ch>
2019-12-05 12:00:19 +0100

    Changed integrated SQL schema script.

--- schema/gdelt.sql
+++ schema/gdelt.sql
@@ -4,82 +4,55 @@ CREATE TABLE IF NOT EXISTS data_management_fields
     SOURCEURL VARCHAR(999) UNIQUE
 );
 
-CREATE TABLE IF NOT EXISTS event_geo_actor1
+CREATE TABLE IF NOT EXISTS event_geo
 (
-    Actor1Geo_ADM1Code    VARCHAR(999) PRIMARY KEY,
-    Actor1Geo_FeatureID   INTEGER,
-    Actor1Geo_CountryCode VARCHAR(999),
-    Actor1_Geo_Lat        FLOAT,
-    Actor1_Geo_Long       FLOAT,
-    Actor1_Geo_Type       INTEGER,
-    Actor1Geo_FullName    VARCHAR(999)
+    ADM1Code    VARCHAR(999) PRIMARY KEY,
+    FeatureID   INTEGER,
+    CountryCode VARCHAR(999),
+    Lat         FLOAT,
+    Long        FLOAT,
+    Type        INTEGER,
+    FullName    VARCHAR(999)
+);
+
+CREATE TABLE IF NOT EXISTS actor
+(
+    Code           VARCHAR(999) PRIMARY KEY,
+    Name           VARCHAR(999),
+    KnownGroupCode VARCHAR(999),
+    Religion1Code  VARCHAR(999),
+    Religion2Code  VARCHAR(999),
+    CountryCode    VARCHAR(999),
+    Type1Code      VARCHAR(999),
+    Type2Code      VARCHAR(999),
+    Type3Code      VARCHAR(999),
+    EthnicCode     VARCHAR(999)
 );
 
 CREATE TABLE IF NOT EXISTS actor1
 (
-    Actor1Code           VARCHAR(999) PRIMARY KEY,
-    Actor1Geo_ADM1Code   VARCHAR(999),
-    Actor1Name           VARCHAR(999),
-    Actor1KnownGroupCode VARCHAR(999),
-    Actor1Religion1Code  VARCHAR(999),
-    Actor1Religion2Code  VARCHAR(999),
-    Actor1CountryCode    VARCHAR(999),
-    Actor1Type1Code      VARCHAR(999),
-    Actor1Type2Code      VARCHAR(999),
-    Actor1Type3Code      VARCHAR(999),
-    Actor1EthnicCode     VARCHAR(999),
+    ADM1Code VARCHAR(999),
+    FOREIGN KEY (ADM1Code) REFERENCES event_geo (ADM1Code)
+) INHERITS (actor);
 
-    FOREIGN KEY (Actor1Geo_ADM1Code) REFERENCES event_geo_actor1 (Actor1Geo_ADM1Code)
-);
 
-CREATE TABLE IF NOT EXISTS event_geo_actor2
-(
-    Actor2Geo_ADM1Code    VARCHAR(999) PRIMARY KEY,
-    Actor2Geo_FeatureID   INTEGER,
-    Actor2Geo_CountryCode VARCHAR(999),
-    Actor2_Geo_Lat        FLOAT,
-    Actor2_Geo_Long       FLOAT,
-    Actor2_Geo_Type       INTEGER,
-    Actor2Geo_FullName    VARCHAR(999)
-);
 
 CREATE TABLE IF NOT EXISTS actor2
 (
-    Actor2Code           VARCHAR(999) PRIMARY KEY,
-    Actor2Geo_ADM1Code   VARCHAR(999),
-    Actor2Name           VARCHAR(999),
-    Actor2KnownGroupCode VARCHAR(999),
-    Actor2Religion1Code  VARCHAR(999),
-    Actor2Religion2Code  VARCHAR(999),
-    Actor2CountryCode    VARCHAR(999),
-    Actor2Type1Code      VARCHAR(999),
-    Actor2Type2Code      VARCHAR(999),
-    Actor2Type3Code      VARCHAR(999),
-    Actor2EthnicCode     VARCHAR(999),
-
-    FOREIGN KEY (Actor2Geo_ADM1Code) REFERENCES event_geo_actor2 (Actor2Geo_ADM1Code)
-);
-
-CREATE TABLE IF NOT EXISTS event_geo_action
-(
-    ActionGeo_ADM1Code    VARCHAR(999) PRIMARY KEY,
-    ActionGeo_FeatureID   INTEGER,
-    ActionGeo_CountryCode VARCHAR(999),
-    Action_Geo_Lat        FLOAT,
-    Action_Geo_Long       FLOAT,
-    Action_Geo_Type       INTEGER,
-    ActionGeo_FullName    VARCHAR(999)
-);
+    ADM1Code VARCHAR(999),
+    FOREIGN KEY (ADM1Code) REFERENCES event_geo (ADM1Code)
+) INHERITS (actor);
 
 CREATE TABLE IF NOT EXISTS country
 (
-    Geo VARCHAR(99) PRIMARY KEY
+    CID SERIAL PRIMARY KEY,
+    Geo VARCHAR(99) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS income
 (
     IID      SERIAL PRIMARY KEY,
-    Geo      VARCHAR(99) NOT NULL,
+    CID      SERIAL NOT NULL,
     Value    INTEGER,
     Unit     VARCHAR(99),
     AgeGroup VARCHAR(999),
@@ -87,22 +60,22 @@ CREATE TABLE IF NOT EXISTS income
     Year     INTEGER,
     Sex      VARCHAR(999),
 
-    FOREIGN KEY (Geo) REFERENCES country (Geo)
+    FOREIGN KEY (CID) REFERENCES country (CID)
 );
 
 CREATE TABLE IF NOT EXISTS tourist
 (
-    TID          SERIAL PRIMARY KEY,
-    Geo          VARCHAR(99) NOT NULL,
-    Value        INTEGER,
-    Time         INTEGER,
-    Accomodation VARCHAR(999),
-    Unit         VARCHAR(999),
+    TID           SERIAL PRIMARY KEY,
+    CID           SERIAL NOT NULL,
+    Value         INTEGER,
+    Time          INTEGER,
+    Accommodation VARCHAR(999),
+    Unit          VARCHAR(999),
 
-    FOREIGN KEY (Geo) REFERENCES country (Geo)
+    FOREIGN KEY (CID) REFERENCES country (CID)
 );
 
-CREATE TABLE IF NOT EXISTS influence
+CREATE TABLE IF NOT EXISTS influence_income
 (
     TID SERIAL,
     IID SERIAL,
@@ -113,24 +86,24 @@ CREATE TABLE IF NOT EXISTS influence
 
 CREATE TABLE IF NOT EXISTS event_action
 (
-    EventCode          VARCHAR(999) PRIMARY KEY,
-    Actor1Code         VARCHAR(999),
-    Actor2Code         VARCHAR(999),
-    ActionGeo_ADM1Code VARCHAR(999),
-    TID                INTEGER,
-    EventBaseCode      VARCHAR(999),
-    EventRootCode      VARCHAR(999),
-    IsRootEvent        BOOLEAN,
-    GoldsteinScale     FLOAT,
-    QuadClass          INTEGER,
-    AvgTone            FLOAT,
-    NumMentions        INTEGER,
-    NumSources         INTEGER,
-    NumArticles        INTEGER,
+    EventCode      VARCHAR(999) PRIMARY KEY,
+    Actor1Code     VARCHAR(999),
+    Actor2Code     VARCHAR(999),
+    ADM1Code       VARCHAR(999),
+    TID            INTEGER,
+    EventBaseCode  VARCHAR(999),
+    EventRootCode  VARCHAR(999),
+    IsRootEvent    BOOLEAN,
+    GoldsteinScale FLOAT,
+    QuadClass      INTEGER,
+    AvgTone        FLOAT,
+    NumMentions    INTEGER,
+    NumSources     INTEGER,
+    NumArticles    INTEGER,
 
-    FOREIGN KEY (Actor1Code) REFERENCES actor1 (Actor1Code),
-    FOREIGN KEY (Actor2Code) REFERENCES actor2 (Actor2Code),
-    FOREIGN KEY (ActionGeo_ADM1Code) REFERENCES event_geo_action (ActionGeo_ADM1Code),
+    FOREIGN KEY (Actor1Code) REFERENCES actor1 (Code),
+    FOREIGN KEY (Actor2Code) REFERENCES actor2 (Code),
+    FOREIGN KEY (ADM1Code) REFERENCES event_geo (ADM1Code),
     FOREIGN KEY (TID) REFERENCES tourist (TID)
 );
 
